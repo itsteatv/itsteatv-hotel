56d9c3e80abcb2a3e70c92562e23f9692fff2c57
itsteatv <rezashow60@gmail.com>
2023-08-26 23:36:17 +0330

    dropdown items changed

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -83,30 +83,21 @@ function Table() {
                                             className="z-50 hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700"
                                             aria-labelledby="hs-dropdown-custom-icon-trigger"
                                         >
-                                            <a
+                                            <button
                                                 className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                                                href="#"
                                             >
-                                                Newsletter
-                                            </a>
-                                            <a
+                                                Duplicate
+                                            </button>
+                                            <button
                                                 className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                                                href="#"
                                             >
-                                                Purchases
-                                            </a>
-                                            <a
+                                                Edit
+                                            </button>
+                                            <button
                                                 className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                                                href="#"
                                             >
-                                                Downloads
-                                            </a>
-                                            <a
-                                                className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                                                href="#"
-                                            >
-                                                Team Account
-                                            </a>
+                                                Delete
+                                            </button>
                                         </div>
                                     </div>
                                 </td>
