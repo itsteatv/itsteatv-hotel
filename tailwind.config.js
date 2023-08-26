2ae7865b6b4ab832459f42252dbdb640903f4224
itsteatv <rezashow60@gmail.com>
2023-08-26 23:18:08 +0330

    tailwind configuration updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,18 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "mobile": { "min": "15.625em", "max": "43.75em" }
+      }
+    },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require('preline/plugin'),
   ]
 }
