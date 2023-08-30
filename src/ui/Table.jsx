9efb4d62af12450620082dcd2068fdd94b458e33
itsteatv <rezashow60@gmail.com>
2023-08-30 11:28:49 +0330

    bug in opening modal fixed

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -24,7 +24,7 @@ function Table() {
             })
         },
 
-        onError: (error) => {
+        onError: () => {
             toast.error("Cabin Could not be deleted", {
                 icon: <GiChoppedSkull />,
             })
@@ -118,7 +118,7 @@ function Table() {
                                                     Duplicate
                                                 </button>
                                                 <button
-                                                    onClick={() => setShowForm((show) => !show)}
+                                                    onClick={() => setShowForm(true)}
                                                     type="button"
                                                     data-hs-overlay="#hs-modal-signup"
                                                     className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
