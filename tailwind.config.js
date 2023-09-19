9ca25447d4a98bfba223e5b77ca5f075ebf3c1fa
itsteatv <rezashow60@gmail.com>
2023-09-20 03:01:35 +0330

    media queries sizes changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,14 @@ export default {
   theme: {
     extend: {
       screens: {
-        "mobile": { "min": "15.625em", "max": "48.75em" },
+        "mobile": { "min": "9.375em", "max": "48.75em" },
+        "bookingTable": { "min": "9.375em", "max": "56.25em" },
         "filteringDiscount": { "min": "9.375em", "max": "40em" },
         "settingForm": { "min": "9.375em", "max": "37.5em" },
       },
       fontSize: {
-        clamp: "clamp(0.75rem, 2.3vw + 0.5rem, 1.875rem)"
+        clamp: "clamp(0.75rem, 2.3vw + 0.5rem, 1.875rem)",
+        dateFont: "clamp(0.5rem, 1.6vw + 0.3rem, 1rem)"
       }
     },
   },
