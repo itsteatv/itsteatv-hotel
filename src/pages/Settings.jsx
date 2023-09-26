e452aaa29e555b7ba66585ad3c7e8ec2f8c17701
itsteatv <rezashow60@gmail.com>
2023-09-26 06:29:24 +0330

    ui improvement

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,7 +3,7 @@ import UpdateSettingForm from "../settings/UpdateSettingForm"
 function Settings() {
   return (
     <div>
-      <h1 className="text-clamp text-center mt-16 text-3xl font-semibold">Update hotel setting</h1>
+      <h1 className="text-clamp text-center mt-16 text-3xl font-semibold dark:text-white">Update hotel setting</h1>
       <UpdateSettingForm />
     </div>
   )
