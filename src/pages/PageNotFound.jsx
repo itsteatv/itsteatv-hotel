ef4c3bfe4605261ae3470597e25d49a466907b3e
itsteatv <rezashow60@gmail.com>
2023-10-06 00:37:27 +0330

    i fix a small stupid bug

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,24 +1,41 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 
 function PageNotFound() {
+
+  const navigate = useNavigate();
+
+  const handleGoBackToDashboard = () => {
+    navigate('/');
+  };
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
   return (
     <main>
-      <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
+      <div className="bg-red-700 max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
         <div className="max-w-lg mx-auto space-y-3 text-center">
-          <h3 className="text-indigo-600 font-semibold">
+          <h3 className="text-white font-semibold">
             404 Error
           </h3>
-          <p className="text-gray-800 text-4xl font-semibold sm:text-5xl">
+          <p className="text-black text-4xl font-extrabold sm:text-5xl">
             Page not found
           </p>
-          <p className="text-gray-600">
+          <p className="text-black">
             Sorry, the page you are looking for could not be found or has been removed.
           </p>
           <div className="flex flex-wrap items-center justify-center gap-3">
-            <Link to="/" className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg">
+            <button
+              onClick={() => {
+                handleGoBackToDashboard();
+                reloadPage();
+              }}
+              className="block py-2 px-4 text-white font-medium bg-black duration-150 hover:bg-gray-50 hover:text-black active:bg-gray-100 active:text-black rounded-lg"
+            >
               Go back
-            </Link>
-            <a href="https://github.com/itsteatv" target="_blank" rel="noreferrer" className="block py-2 px-4 text-gray-700 hover:bg-gray-50 font-medium duration-150 active:bg-gray-100 border rounded-lg">
+            </button>
+            <a href="https://github.com/itsteatv" target="_blank" rel="noreferrer" className="block py-2 px-4 text-black hover:bg-gray-50 font-medium duration-150 active:bg-gray-100 border rounded-lg dark:text-white">
               Contact support
             </a>
           </div>
