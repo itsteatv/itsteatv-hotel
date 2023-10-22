c790caf5e2fca42647690702d629ddba55903505
itsteatv <rezashow60@gmail.com>
2023-10-22 06:34:30 +0330

    removing unnecessary code

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { login as loginApi } from "../services/apiAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
