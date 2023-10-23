467190d2ff55f618f7cd5c72a9c0c7a4282176ad
itsteatv <rezashow60@gmail.com>
2023-10-23 06:49:33 +0330

    i forgot App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import { Toaster } from "react-hot-toast";
 import CheckIn from "./pages/CheckIn";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -29,7 +30,7 @@ function App() {
             <ReactQueryDevtools initialIsOpen={false} />
             <BrowserRouter>
                 <Routes>
-                    <Route element={<AppLayout />}>
+                    <Route element={<ProtectedRoute> <AppLayout /> </ProtectedRoute>}>
                         <Route index element={<Navigate replace to="dashboard" />} />
                         <Route path="dashboard" element={<Dashboard />} />
                         <Route path="bookings" element={<Bookings />} />
@@ -39,9 +40,9 @@ function App() {
                         <Route path="users" element={<Users />} />
                         <Route path="settings" element={<Settings />} />
                         <Route path="account" element={<Account />} />
-                        <Route path="login" element={<Login />} />
                     </Route>
 
+                    <Route path="login" element={<Login />} />
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
