72940dd23a8042ad6af66c06434aacf705e73d3b
itsteatv <rezashow60@gmail.com>
2023-10-24 06:08:37 +0330

    i forgot to remove sidebar from app layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import Header from "./Header"
-import Sidebar from "./Sidebar"
 
 function AppLayout() {
     return (
         <div className='h-screen'>
             <Header />
-            <Sidebar />
             <main>
                 <Outlet />
             </main>
