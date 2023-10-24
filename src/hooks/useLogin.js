0271728bd4e0e7637f6cf0bbc3df6e6ca38ae0ec
itsteatv <rezashow60@gmail.com>
2023-10-25 02:38:04 +0330

    i said login had a bug , i think now it's fixed

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,15 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../services/apiAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
 
     const { isLoading, mutate: login } = useMutation({
         mutationFn: ({ email, password }) => loginApi({ email, password }),
 
-        onSuccess: () => {
+        onSuccess: (user) => {
+            console.log(user);
+
+            queryClient.setQueryData(["user"], user.user)
+
             toast.success("You've been successfully logged in"),
                 navigate("/dashboard", { replace: true })
         },
