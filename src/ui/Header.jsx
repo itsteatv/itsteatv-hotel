2a289ba8be111c4cbddda2a7eb05b68259b580ea
itsteatv <rezashow60@gmail.com>
2023-10-31 06:36:51 +0330

    small ui fix in header for mobiles

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -84,7 +84,7 @@ function Header() {
           >
             <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
               {/* DROPDOWN */}
-              <div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:hover] sm:py-4">
+              <div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:click] sm:py-4">
                 <button
                   type="button"
                   className="flex items-center w-full text-gray-500 hover:text-gray-400 font-medium px-6 sm:px-0 dark:text-gray-400 dark:hover:text-gray-500"
