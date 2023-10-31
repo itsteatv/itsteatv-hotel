cd53fc5bd59c4ad55af9a9664dd4b0760afa722e
itsteatv <rezashow60@gmail.com>
2023-10-31 06:53:41 +0330

    again fixing ui

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -4,9 +4,9 @@ import UpdateUserPasswordForm from "../ui/UpdateUserPasswordForm"
 function Account() {
   return (
     <div>
-      <h1 className="text-clamp text-center mt-16 text-3xl font-semibold font-Ubuntu dark:text-white">Update user setting</h1>
+      <h1 className="text-clamp text-center mt-10 text-3xl font-semibold font-Ubuntu dark:text-white">Update user setting</h1>
       <UpdateUserDataForm />
-      <h1 className="text-clamp text-center mt-16 text-3xl font-semibold font-Ubuntu dark:text-white">Update password</h1>
+      <h1 className="text-clamp text-center mt-10 text-3xl font-semibold font-Ubuntu dark:text-white">Update password</h1>
       <UpdateUserPasswordForm />
     </div>
   )
