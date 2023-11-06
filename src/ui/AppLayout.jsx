6a6b3d7d9388a3bdf9f81db89646ca2ace047109
itsteatv <rezashow60@gmail.com>
2023-11-06 22:34:39 +0330

    now i think the zoom is disabled

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,7 +3,7 @@ import Header from "./Header"
 
 function AppLayout() {
     return (
-        <div className='h-screen'>
+        <div className='h-screen touch-pan-x touch-pan-y'>
             <Header />
             <main>
                 <Outlet />
